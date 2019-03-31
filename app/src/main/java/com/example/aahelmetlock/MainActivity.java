11cc8c31ceb1e0625d74093a76aa63caba001ba6
Allan Murillo <noelmurillo15@live.com>
2019-03-31 13:34:52 -0700

    Decreased Unlock/Lock Button Enable Timer
    -

--- app/src/main/java/com/example/aahelmetlock/MainActivity.java
+++ app/src/main/java/com/example/aahelmetlock/MainActivity.java
@@ -689,7 +689,7 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             });
                         }
-                    }, 2000);
+                    }, 1000);
 
                     mNotifyRead = mBatteryRead;
                     mLeGattService.readCharacteristic(mNotifyRead);
